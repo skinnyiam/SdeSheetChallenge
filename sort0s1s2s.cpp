99c91018583553a1d4e03a8544917b140daf726b
Saurabh Shukla <84450605+skinnyiam@users.noreply.github.com>
2022-06-05 18:46:32 +0530

    First day--first problem

---
+++ sort0s1s2s.cpp
@@ -0,0 +1,21 @@
+//three pointer approach
+//sort 0s,1s,2s....
+
+ void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0, mid = 0, high = n-1;
+        
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[low]);
+                low++; mid++;
+            }
+            else if(nums[mid] == 2){
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+            else{
+                mid++;
+            }
+        }
+    }
