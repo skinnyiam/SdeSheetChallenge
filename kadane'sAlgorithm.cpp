e4871cfd42eac73a90517220e85f2879da9b43b5
Saurabh Shukla <84450605+skinnyiam@users.noreply.github.com>
2022-06-05 19:05:02 +0530

    Firstday---secondProblem

---
+++ kadane'sAlgorithm.cpp
@@ -0,0 +1,21 @@
+//kadane's algorithm
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+        int currsum=0;
+        int maxsum=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            currsum+=nums[i];
+            if(currsum>maxsum){
+             maxsum=currsum;
+            }
+            if(currsum<0){
+              currsum=0;
+            }
+        }
+        return maxsum;
+        
+    }
