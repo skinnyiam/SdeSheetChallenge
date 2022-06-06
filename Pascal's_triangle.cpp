63ec4f5d2ca77bc1088d2c0f2ebe0a61319243e5
Saurabh Shukla <84450605+skinnyiam@users.noreply.github.com>
2022-06-06 19:58:28 +0530

    second day----second problem

--- Pascal's_triangle.cpp
+++ Pascal's_triangle.cpp
@@ -2,7 +2,29 @@
 // 1) if the row number and column number is given we can tell the value by (r-1)^C(c-1). T.C O(N)
 // 2) print the nth row of pascal triangle 
 
-
+ vector<int> getRow(int rowIndex) {
+        vector<vector<int>> ans;
+        
+        for(int i=0;i<rowIndex+1;i++){
+            vector<int> temp(i+1,1);
+                 for(int j=1;j<i;j++){
+                   temp[j]=ans[i-1][j-1]+ans[i-1][j];
+                    
+            }
+            ans.push_back(temp);
+            temp.clear();
+        } 
+        vector<int> res;
+        for(int i=0;i<ans.size();i++){
+             res.clear();
+            for(int j=0;j<ans[i].size();j++){
+                res.push_back(ans[i][j]);
+               
+            }
+        }
+        
+        return res;
+    }
 
 
 
