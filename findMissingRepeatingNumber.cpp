3c8c01e752994deb515a6c5e9663c1ddf8b5d8ab
Saurabh Shukla <84450605+skinnyiam@users.noreply.github.com>
2022-06-07 21:40:34 +0530

    third day---second problem

--- findMissingRepeatingNumber.cpp
+++ findMissingRepeatingNumber.cpp
@@ -1,1 +1,28 @@
-// first method
+// first method--hashing
+pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+{
+	// Write your code here 
+	pair<int,int> p;
+    int ar[n+1]={0};
+    for(auto it:arr){
+        ar[it]++;
+    }
+    for(int i=1;i<n+1;i++){
+        if(ar[i]>=2){
+            p.second=i;
+        }
+        if(ar[i]==0){
+            p.first=i;
+        }
+    }
+    
+//     cout<<repeat<<endl;
+    
+    return p;
+}
+
+tc--O(N)
+SC---O(N)
+  
+  //optimal approach is doing xor 
+  
