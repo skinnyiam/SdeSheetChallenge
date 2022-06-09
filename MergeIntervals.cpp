1b9a1bc34ab6023aaba3c54669a3992e705de141
Saurabh Shukla <84450605+skinnyiam@users.noreply.github.com>
2022-06-09 17:10:59 +0530

    fourthDay--first problem

---
+++ MergeIntervals.cpp
@@ -0,0 +1,25 @@
+ vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        if(n<=1){
+            return intervals;
+        }
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
+        
+        for(int i=0;i<n;i++){
+            if(ans.empty()){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                vector<int> &v=ans.back();
+                int y=v[1];
+                if(intervals[i][0]<=y){
+                    v[1] = max(y,intervals[i][1]);
+                }else{
+                    ans.push_back(intervals[i]);
+                }
+            }
+        }
+        
+        return ans;
+    }
