b98d5e51d1760d8b08b0d9a27ad32597f2306d92
Saurabh Shukla <84450605+skinnyiam@users.noreply.github.com>
2022-06-13 21:14:30 +0530

    fourthDay-thirdProblem

---
+++ majorityEl1.cpp
@@ -0,0 +1,22 @@
+ int majorityElement(vector<int>& nums) {
+        /*
+        map<long long,long long> m;
+        int n= nums.size();
+        for(int i=0;i<n;i++){
+m[nums[i]]++;
+        }
+        for(auto it:m) if(it.second>(n/2)) return it.first ;
+        */
+   
+   //O(N)
+        int count=0;
+        int numbers=0;
+        for(int num:nums){
+            if(count==0){
+                numbers=num;
+            }
+            if(num==numbers) count++;
+            else count-=1;
+        }
+        return numbers;
+    }
