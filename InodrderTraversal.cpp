47a483ebb7d8e5b794259dd77bce3915086da7ff
Saurabh Shukla <84450605+skinnyiam@users.noreply.github.com>
2022-06-27 16:37:48 +0530

    Update InodrderTraversal.cpp

--- InodrderTraversal.cpp
+++ InodrderTraversal.cpp
@@ -1,22 +1,36 @@
-class Solution {
 public:
+    
+    void recursive(TreeNode* root, vector<int>& inorder){
+        if(root==NULL) return;
+        recursive(root->left,inorder);
+        inorder.push_back(root->val);
+        recursive(root->right,inorder);
+    }
+    
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> inorder;
-        stack<TreeNode*> st;
-        TreeNode* node=root;
-        while(true){
-            if(node!=NULL){
-                st.push(node);
-                node=node->left;
-            }else{
-                if(st.empty()==true) break;
-                node=st.top();
-                st.pop();
-                inorder.push_back(node->val);
-                node=node->right;
-                
-            }
-        }
+        
+        //iterative
+        // vector<int> inorder;
+        // stack<TreeNode*> st;
+        // TreeNode* node=root;
+        // while(true){
+        //     if(node!=NULL){
+        //         st.push(node);
+        //         node=node->left;
+        //     }
+        //     else{
+        //         if(st.empty()==true) break;
+        //         node=st.top();
+        //         st.pop();
+        //         inorder.push_back(node->val);
+        //         node=node->right;
+        //     }
+        // }
+        // return inorder;
+        
+        //recursive
+         vector<int> inorder;
+        recursive(root,inorder);
         return inorder;
     }
 };
